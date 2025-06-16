ff35f7fe78d047ebf40108f5224d527390aa2266
manikachaudhary <cmanika23@gmail.com>
2025-06-16 13:54:30 -0500

    Initial commit - setup

---
+++ portfolio-service/src/test/java/com/pulsetrade/portfolio_service/PortfolioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulsetrade.portfolio_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
